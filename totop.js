10aa76823b929ce5ab40773597958124d22f61b7
du.xiong <duxn1989@163.com>
2016-10-19 10:19:32 +0800

    Site updated: 2016-10-19 10:19:32

--- totop.js
+++ totop.js
@@ -9,13 +9,13 @@
     $(window).scroll(function () {
         var scrollTop = $(document).scrollTop();
         if ( scrollTop > upperLimit ) {
-            $(scrollElem).stop().fadeIn(300);
+            scrollElem.stop().fadeIn(300);
         }else{
-            $(scrollElem).stop().fadeOut(300);
+            scrollElem.stop().fadeOut(300);
         }
     });
 
-    $(scrollElem).click(function(){
+    scrollElem.click(function(){
         $('html, body').animate({scrollTop:0}, scrollSpeed); return false;
     });
 })(jQuery);
