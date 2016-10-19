11e3e631ba72504b394a6e372885c31275dadd85
du.xiong <duxn1989@163.com>
2016-10-19 09:55:41 +0800

    Site updated: 2016-10-19 09:55:41

--- totop.js
+++ totop.js
@@ -1,22 +1,21 @@
 (function($) {
-    var upperLimit = 300;
+    var upperLimit = 1000;
 
     var scrollElem = $('#totop');
 
-    var scrollSpeed = 500;
+    var scrollSpeed = 1600;
 
     scrollElem.hide();
     $(window).scroll(function () {
         var scrollTop = $(document).scrollTop();
         if ( scrollTop > upperLimit ) {
-            $(scrollElem).fadeIn(300, 1);
+            $(scrollElem).stop().fadeTo(300, 1);
         }else{
-            $(scrollElem).fadeOut(300, 0);
+            $(scrollElem).stop().fadeTo(300, 0);
         }
     });
 
     $(scrollElem).click(function(){
         $('html, body').animate({scrollTop:0}, scrollSpeed); return false;
     });
-	console.log(0)
 })(jQuery);
